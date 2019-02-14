1287b16591cff0fc4c527bb9b5661b5e6c045638
Conanap <albionapc@gmail.com>
2019-02-14 01:38:10 -0500

    damn

--- Distro/MiniMax_search.c
+++ Distro/MiniMax_search.c
@@ -242,7 +242,7 @@ double MiniMax(double gr[graph_size][4], int path[1][2], double minmax_cost[size
 			{ // we updated ma boi
 				path[0][0] = new_mouse_loc[0][0];
 				path[0][1] = new_mouse_loc[0][1];
-				fprintf(stderr, "\tNew path selected: @(%d, %d)\n", path[0][0], path[0][1]);
+				fprintf(stderr, "\tNew path selected: @(%d, %d), depth %d\n", path[0][0], path[0][1], depth);
 
 				minmax_cost[mouse_loc[0][0]][mouse_loc[0][1]] = ret;
 				alpha = max(alpha, ret);
